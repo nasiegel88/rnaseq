238df69ec8a2be7c6caed051d1010e7f88676e85
nasiegel88 <nasiegel@ucdavis.edu>
2020-10-20 15:10:55 -0700

    Update download-ref.sh

---
+++ mouse_ref/download-ref.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget ftp://ftp.ensembl.org/pub/release-101/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz
+
+wget ftp://ftp.ensembl.org/pub/release-101/gtf/mus_musculus/Mus_musculus.GRCm38.101.gtf.gz
