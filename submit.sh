23d70377f2aa5ccc1fd90781726b7442f9e1cab7
nasiegel88 <nasiegel@ucdavis.edu>
2020-10-21 10:57:54 -0700

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -3,7 +3,7 @@
 #SBATCH -J ets04               # name for job
 #SBATCH -N 1                   # one "node", or computer
 #SBATCH -n 1                   # one task for this node
-#SBATCH -c 8                   # eight cores per task
+#SBATCH -c 10                   # eight cores per task
 #SBATCH -t 8:00:00             # ask for no more than 30 minutes
 #SBATCH --mem=20G             # ask for no more than 10 GB of memory
 #SBATCH -o jobname-%j.out  # send stdout to outfile
@@ -26,7 +26,7 @@ set -x
 
 # run the snakemake!
 # Select which snakefile you want to submit
-snakemake --use-conda
+snakemake -j 10 --use-conda
 
 # print out various information about the job
 env | grep SLURM            # Print out values of the current jobs SLURM environment variables
