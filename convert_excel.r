020c6fa734c4ce57b188e53f8bd31abd75294daf
nasiegel88 <nasiegel@ucdavis.edu>
2021-01-07 12:46:39 -0800

    clean commits

--- convert_excel.r
+++
@@ -1,6 +0,0 @@
-library(openxlsx)
-# The ```assay``` function allows us to covert deseq2 objects into data tables
-data = assay(se)
-# The ```write.xlsx``` function allows to convert datatables into to excel files
-write.xlsx(data, file='se.xlsx', sheetName="Sheet1",
-           col.names=TRUE, row.names=TRUE, append=FALSE)
